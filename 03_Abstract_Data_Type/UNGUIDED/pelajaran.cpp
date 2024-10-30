2ed7dcf477bda8cd184d9df76d3dd743611b6db1
Marsella dwi julianti <146401214+marsellacell@users.noreply.github.com>
2024-10-30 20:36:56 +0700

    Add files via upload

---
+++ 03_Abstract_Data_Type/UNGUIDED/pelajaran.cpp
@@ -0,0 +1,14 @@
+// #include "pelajaran.h"
+// #include <iostream>
+
+// pelajaran create_pelajaran(const std::string& namaMapel, const std::string& kodeMapel) {
+//     pelajaran pel;
+//     pel.namaMapel = namaMapel;
+//     pel.kodeMapel = kodeMapel;
+//     return pel;
+// }
+
+// void tampil_pelajaran(const pelajaran& pel) {
+//     std::cout << "Nama Mapel: " << pel.namaMapel << std::endl;
+//     std::cout << "Kode Mapel: " << pel.kodeMapel << std::endl;
+// }
